f91002b4dfe40a9f0704feb167ce45ae23e1ea34
Tyler Lewis <tlewis4@intuit.com>
2017-03-31 09:52:23 -0700

    Initial Commit

---
+++ flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flicks
+//
+//  Created by Tyler Hackley Lewis on 3/31/17.
+//  Copyright © 2017 Tyler Hackley Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
