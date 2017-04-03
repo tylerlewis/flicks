9730bb6b4071356324154e26e4262ef17b644bfd
Tyler Lewis <tlewis4@intuit.com>
2017-04-02 22:34:09 -0700

    resize movie detail box dynamically

--- flicks/MovieDetailViewController.swift
+++ flicks/MovieDetailViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
     
+    @IBOutlet weak var movieDetailScrollView: UIScrollView!
+    
+    @IBOutlet weak var movieDetailView: UIView!
+    
     @IBOutlet weak var movieTitleLabel: UILabel!
     
     @IBOutlet weak var movieReleseDateLabel: UILabel!
@@ -28,6 +32,15 @@ class MovieDetailViewController: UIViewController {
         movieTitleLabel.text = movieTitle ?? ""
         movieReleseDateLabel.text = movieReleaseDate ?? ""
         movieOverviewLabel.text = movieOverview ?? ""
+        movieOverviewLabel.sizeToFit()
+        
+        // Resize the movieDetailView based on size of overview label
+        movieDetailView.frame.size = CGSize(width: movieDetailView.frame.width, height: movieDetailView.frame.height + movieOverviewLabel.frame.height)
+        
+        let heightMultiple = Float(1 + (movieOverviewLabel.numberOfLines / 20))
+        let contentWidth = movieDetailScrollView.bounds.width
+        let contentHeight = Float(movieDetailScrollView.bounds.height) * heightMultiple
+        movieDetailScrollView.contentSize = CGSize(width: contentWidth, height: CGFloat(contentHeight))
     }
 
     override func didReceiveMemoryWarning() {
